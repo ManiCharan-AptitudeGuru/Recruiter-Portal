89038cad9ffb5fb4e151cbe8c2cde2343a5077be
ManiCharan-AptitudeGuru <manicharanreddy@aptitudeguruhem.com>
2024-09-26 14:51:27 +0530

    Updated API

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 //https://recruiter-portal.onrender.com
 //http://localhost:5000
-const API_URL = "http://localhost:5000";
+const API_URL = "https://recruiter-portal.onrender.com";
 
 export const api = axios.create({
   baseURL: API_URL,
--- Server/app.js
+++ Server/app.js
@@ -8,10 +8,10 @@ const templatesRoutes = require("./routes/templates");
 const recruiterRoutes = require("./routes/recruiterRoutes");
 const premiumPlansRoutes = require("./routes/plans");
 const invoiceRoutes = require("./routes/invoice");
-const reportRoutes = require('./routes/reportRoutes');
-const createReportRoutes=require('./routes/createReportRoute')
-const exportRoutes=require('./routes/exportRoutes')
-const scheduleRoute=require('./routes/scheduleRoutes')
+const reportRoutes = require("./routes/reportRoutes");
+const createReportRoutes = require("./routes/createReportRoute");
+const exportRoutes = require("./routes/exportRoutes");
+const scheduleRoute = require("./routes/scheduleRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const { connectDB } = require("./config/db");
 const { initCronJobs } = require("./utils/cronJobs");
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://recruiter-portal-theta.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -43,7 +43,7 @@ app.use("/templates", templatesRoutes);
 app.use("/recruiters", recruiterRoutes);
 app.use("/premium-plans", premiumPlansRoutes);
 app.use("/", invoiceRoutes);
-app.use('/' , reportRoutes);
+app.use("/", reportRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
